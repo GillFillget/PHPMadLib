341cb68c04a7324b1f0833829fdb4d053b27c473
GillFillget <bretm1002@ksu.edu>
2022-09-29 13:26:42 -0500

    created files

---
+++ madlib.php
@@ -0,0 +1,11 @@
+<html>
+    <body>
+        <h1>THE MAGIC COMPUTERS</h1>
+        <h3>Today, every student has a computer small enough to fit into his <?php echo $_POST["NOUN"]; ?>.
+        <br> He can solve any math problem by simply pushing the computer's little <?php echo $_POST["PNOUN"]; ?>.
+        <br> Computers can add, multiply, divide, and <?php echo $_POST["VERB"]; ?>. They can also <?php echo $_POST["VERB2"]; ?> better than a human.
+        <br> Some computers are <?php echo $_POST["BODYPART"]; ?>. Others have a/an <?php echo $_POST["ADJECTIVE"]; ?> screen that shows all kinds of 
+        <?php echo $_POST["PNOUN2"]; ?> and <?php echo $_POST["ADJECTIVE2"]; ?> figures.
+        </h3>
+    </body>
+</html>
